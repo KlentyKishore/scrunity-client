c2ca93e95ccff34f1e63248febffebb81c969709
klentynabeel <nabeel@klenty.com>
2023-07-19 15:46:26 +0530

    new client repo changes

--- models/index.js
+++
@@ -1,5 +0,0 @@
-const User= require("./User")
-const Question= require("./Question")
-const EditAccess= require("./EditAccess")
-const ImpactFolder= require("./ImpactFolder.js")
-module.exports = {User,Question,EditAccess,ImpactFolder};
